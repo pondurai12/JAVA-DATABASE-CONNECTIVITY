52014463ca983cb37d21e593690abf86f5feef11
pondurai12 <pdurai31@gmail.com>
2019-04-18 22:45:04 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -6,54 +6,65 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
 public class Test {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		//Step 1: Load the driver(It automatically registered driver called DriverManager Service)
+	public static void main(String[] args) {
 		
-		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection connection=null;
+		Statement statement=null;
 		
-		System.out.println("Driver Loading process completed.....");
-		
-		//Step 2:create connection
-		
-		Connection connection =DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "System", "manager");
-		
-		System.out.println("Connection created Succesfully"+connection);
-		
-		// Step 3: Write the query
-		
-		//String query2= "create table king(sno number,name varchar2(20),state varchar2(10),pincode number)";
-		
-	String q2 = "insert into king values(1,'hello','tn',636201)";
-	
-	String q3 = "select*from king";
-		
-		//Step 4:Process the query
-		
-	Statement statement = connection.createStatement();
-//		
-	//int x = statement.executeUpdate(query1);
-//		System.out.println("Table created successfully"+x);
-		
-	
-	//Step 4: Execute the query
-		int y = statement.executeUpdate(q2);
-	
-	System.out.println("Table created successfully"+y);
+		try {
+			
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+//			System.out.println(10/0);
+			System.out.println("Driver Loading process completed.....");
+			
+			//Step 2:create connection
+			
+			 connection =DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "System", "manager");
+			
+			System.out.println("Connection created Succesfully"+connection);
+			System.out.println("Resources declaraton");
+			
+			
 
-	ResultSet rs = statement.executeQuery(q3);
-//	while(rs.next()) {
-//		System.out.println(rs.getString(1));
-//		
-//	}
-	System.out.println("executed query ");
-	
-	//Step 5:Close the connection
-	statement.close();
-	connection.close();
-	
-	System.out.println("Connection closed succesfully");
+			
+		String q1 = "insert into king values(3,'ssss','salem',3736)";
+		String q2 = "select*from king";
+		
+			 statement = connection.createStatement();
+			
+			statement.executeUpdate(q1);
+		ResultSet rs=	statement.executeQuery(q2);
+			while(rs.next()) {
+				System.out.println(" "+rs.getString("name")+" "+rs.getString("sno")+" "+rs.getString("pincode"));
+			}
+			
+			statement.execute(q2);
+			
+			System.out.println("data inseerted"+statement.execute(q2));
+			
+		
+			
+		}catch(ClassNotFoundException  | SQLException e) {
+			e.printStackTrace();
+			System.out.println("handled exception");
+			
+		}finally {
+			try {
+				if(connection!=null) {
+		connection.close();
+				}
+				if(statement!=null) {
+		statement.close();
+				}
+		System.out.println("finally resources closed");
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
+}
 }
